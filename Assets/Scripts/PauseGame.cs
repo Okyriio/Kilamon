2a1b5e84d75ec93a05f082dbc691f1bd28c3cf6e
André <a.silva14@hotmail.com>
2021-01-10 19:44:29 +0100

    staring pause menu

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FMOD.Studio;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    
+    
+    // Update is called once per frame
+    
+    
+    public void  Resume()
+    { 
+                                              
+        pauseMenu.SetActive(false);
+                                             
+    }
+    
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+             if (pauseMenu.activeInHierarchy == false)
+             {
+                 pauseMenu.SetActive(true);
+             }
+             else
+             {
+                 pauseMenu.SetActive(false);
+             }
+        }
+       
+    }
+    
+  
+}
